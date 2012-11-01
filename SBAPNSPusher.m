8357bcc63d1e846165db70a3e820190eae18538c
Simon Blommegård <simon@blommegard.se>
2012-11-01 10:52:40 +0100

    Republish bonjour on app active.

--- SBAPNSPusher.m
+++ SBAPNSPusher.m
@@ -20,48 +20,73 @@ void sb_swizzle(Class class, SEL orig, SEL new) {
 
 @interface SBAPNSPusher () <NSNetServiceDelegate>
 @property (nonatomic, strong) NSNetService *netService;
+@property (nonatomic, strong) NSData *token;
+@property (nonatomic, assign) dispatch_queue_t queue;
 
 + (SBAPNSPusher *)_sniffer;
-- (void)_publishWithToken:(NSData *)token;
+- (void)_republish;
+- (void)_applicationDidBecomeActiveNotification:(NSNotification *)notification;
 @end
 
 @implementation SBAPNSPusher
 
+- (void)dealloc {
+  dispatch_release(self.queue);
+}
+
 + (void)start {
+  id<UIApplicationDelegate> appDelegate = [[UIApplication sharedApplication] delegate];
+  
+  // application:didRegisterForRemoteNotificationsWithDeviceToken:
   SEL newSEL = NSSelectorFromString(@"sb_application:didRegisterForRemoteNotificationsWithDeviceToken:");
   IMP newIMP = imp_implementationWithBlock(^(id _self, UIApplication *application, NSData *token) {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
     [_self performSelector:newSEL withObject:application withObject:token];
 #pragma clang diagnostic pop
-    [[SBAPNSPusher _sniffer] _publishWithToken:token];
+    
+    dispatch_async([SBAPNSPusher _sniffer].queue, ^{
+      [[SBAPNSPusher _sniffer] setToken:token];
+      [[SBAPNSPusher _sniffer] _republish];
+    });
   });
-  
-  id<UIApplicationDelegate> appDelegate = [[UIApplication sharedApplication] delegate];
   
   SEL origSEL = @selector(application:didRegisterForRemoteNotificationsWithDeviceToken:);
   Method origMethod = class_getInstanceMethod([appDelegate class], origSEL);
   
   class_addMethod([appDelegate class], newSEL, newIMP, method_getTypeEncoding(origMethod));
   sb_swizzle([appDelegate class], origSEL, newSEL);
+  
+  [[NSNotificationCenter defaultCenter] addObserver:[SBAPNSPusher _sniffer]
+                                           selector:@selector(_applicationDidBecomeActiveNotification:)
+                                               name:UIApplicationDidBecomeActiveNotification
+                                             object:nil];
 }
 
-#pragma mark -
+#pragma mark - Private
 
 + (SBAPNSPusher *)_sniffer {
   static dispatch_once_t onceToken;
-  static SBAPNSPusher *spoofer;
+  static SBAPNSPusher *sniffer;
   
   dispatch_once(&onceToken, ^{
-    spoofer = [SBAPNSPusher new];
+    sniffer = [SBAPNSPusher new];
   });
   
-  return spoofer;
+  return sniffer;
 }
 
-- (void)_publishWithToken:(NSData *)token {
-  [self.netService setTXTRecordData:[NSNetService dataFromTXTRecordDictionary:@{@"token":token}]];
+- (void)_republish {
+  [self.netService stop];
+  [self.netService setTXTRecordData:[NSNetService dataFromTXTRecordDictionary:@{@"token":self.token}]];
   [self.netService publish];
+}
+
+- (void)_applicationDidBecomeActiveNotification:(NSNotification *)notification {
+  dispatch_async([SBAPNSPusher _sniffer].queue, ^{
+    if ([SBAPNSPusher _sniffer].token)
+      [[SBAPNSPusher _sniffer] _republish];
+  });
 }
 
 #pragma mark - Properties
@@ -72,6 +97,14 @@ void sb_swizzle(Class class, SEL orig, SEL new) {
     [_netService setDelegate:self];
   }
   return _netService;
+}
+
+- (dispatch_queue_t)queue {
+  if (!_queue) {
+    _queue = dispatch_queue_create("se.simonb.SBAPNSPusherQueue", NULL);
+    dispatch_set_target_queue(_queue, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0));
+  }
+  return _queue;
 }
 
 @end
