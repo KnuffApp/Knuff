633bc43f2a21cef470ca07508d9394b10fafd3e7
Prince <wpsteak@gmail.com>
2013-06-30 08:51:44 +0800

    add "iOS APNS Pusher" target

---
+++ iOS APNS Pusher/SBViewController.m
@@ -0,0 +1,29 @@
+//
+//  SBViewController.m
+//  iOS APNS Pusher
+//
+//  Created by Prince on 13/6/30.
+//  Copyright (c) 2013年 Doubleint. All rights reserved.
+//
+
+#import "SBViewController.h"
+
+@interface SBViewController ()
+
+@end
+
+@implementation SBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iOS APNS Pusher/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS APNS Pusher
+//
+//  Created by Prince on 13/6/30.
+//  Copyright (c) 2013年 Doubleint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SBAppDelegate class]));
+    }
+}
