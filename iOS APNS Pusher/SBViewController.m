0ffb92686a2013b8d520f5caba16777bdf83643b
Prince <wpsteak@gmail.com>
2013-06-30 09:17:49 +0800

    now can send push notification to specific device

--- iOS APNS Pusher/SBViewController.m
+++ iOS APNS Pusher/SBViewController.m
@@ -8,11 +8,39 @@
 
 #import "SBViewController.h"
 
+NSString * const kPBAppDelegateDefaultPayload = @"{\n\t\"aps\":{\n\t\t\"alert\":\"Test\",\n\t\t\"sound\":\"default\",\n\t\t\"badge\":0\n\t}\n}";
+
 @interface SBViewController ()
 
 @end
 
 @implementation SBViewController
+
+- (NSDictionary *)payload:(NSString *)payloadStr {
+    NSData *data = [payloadStr dataUsingEncoding:NSUTF8StringEncoding];
+    
+    if (data) {
+        NSDictionary *payload = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
+        if (payload && [payload isKindOfClass:[NSDictionary class]])
+            return payload;
+    }
+    
+    return nil;
+}
+
+#pragma - public
+
+- (IBAction)pushPayload:(UIButton *)sender {
+    
+    NSDictionary *payload = [self payload:kPBAppDelegateDefaultPayload];
+
+    NSString *token = @"3d28a4ae 0a8fb602 1d50edfe ead628a1 5159584c b7efcdf5 ece058c2 23d518da";
+
+    NSDictionary *dict = @{@"token": token ,@"payload":payload};
+    [[NSNotificationCenter defaultCenter] postNotificationName:kPushNotificationKey object:dict];
+}
+
+#pragma - mark view life cycle
 
 - (void)viewDidLoad
 {
